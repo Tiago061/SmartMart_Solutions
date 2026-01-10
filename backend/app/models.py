dc67ca5d4a4acc24fb7b1c6c06616b712db11ba1
Tiago061 <thenriqueleite277@gmail.com>
2026-01-10 17:12:55 -0300

    Add initial models for Category, Product, and Sale

---
+++ backend/app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from database import Base
+
+class Category(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Float)
+    category_id = Column(Integer, ForeignKey("categories.id"))
+
+class Sale(Base):
+    __tablename__ = "sales"
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer)
+    month = Column(Integer)
+    quantity = Column(Integer)
+    total_price = Column(Float)
